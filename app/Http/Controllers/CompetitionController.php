879f561bc43c345aca10d5ac7bba2033177aec14
gouraikawtar <k.gourai14@gmail.com>
2021-05-15 19:32:59 +0000

    added competition model, migration, controller

---
+++ app/Http/Controllers/CompetitionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Competition;
+use Illuminate\Http\Request;
+
+class CompetitionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Competition  $competition
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Competition $competition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Competition  $competition
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Competition $competition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Competition  $competition
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Competition $competition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Competition  $competition
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Competition $competition)
+    {
+        //
+    }
+}
