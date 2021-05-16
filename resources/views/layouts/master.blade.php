294bfc912e13c4c2308c81f2cb9055f7e5e76b92
Salah Eddine Zemmouri <zemm.salaheddine@gmail.com>
2021-05-16 13:13:12 +0100

    added HTTPS for generating ULR assets

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hackathon</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <link href= {{asset('css/app.css')}} rel="stylesheet">
+    <link href= {{secure_asset('css/app.css')}} rel="stylesheet">
 </head>
 <body>
     <div class="container">
