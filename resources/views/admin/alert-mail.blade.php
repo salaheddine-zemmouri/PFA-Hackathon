3713d85f290d9324f0893e27ebd66ce892ce802a
gouraikawtar <k.gourai14@gmail.com>
2021-06-12 07:29:33 +0100

    edited invitation mail blade and alert mail blade

--- resources/views/admin/alert-mail.blade.php
+++ resources/views/admin/alert-mail.blade.php
@@ -6,9 +6,9 @@ You are already member on the plateform, login to access.
 
 # How to get started
 
-1- Go to <b><a href="http://127.0.0.1:8000/login">Hackathon Login Page</a></b><br>
-2- <b>Login</b> using your credentials<br>
-3- That's your dashboard. Go to <b>{{$details['competition_name']}}</b><br>
+1. Go to <b><a href="http://127.0.0.1:8000/login">Hackathon Login Page</a></b> <br>
+1. <b>Login</b> using your credentials <br>
+1. That's your dashboard. Go to <b>{{$details['competition_name']}}</b> <br>
 
 Thanks,<br>
 {{ config('app.name') }}
--- resources/views/admin/invitation-mail.blade.php
+++ resources/views/admin/invitation-mail.blade.php
@@ -6,10 +6,10 @@ There are your credentials to login.
 
 # How to get started
 
-1- Go to <b><a href="http://127.0.0.1:8000/login">Hackathon Login Page</a></b><br>
-2- Enter your email: <b>{{$details['evaluator_email']}}</b> <br>
-3- Enter your password: <b>{{$details['evaluator_password']}}</b> <br>
-4- That's your dashboard. Go to <b>{{$details['competition_name']}}</b> <br>
+1. Go to <b><a href="http://127.0.0.1:8000/login">Hackathon Login Page</a></b> <br>
+1. Enter your email: <b>{{$details['evaluator_email']}}</b> <br>
+1. Enter your password: <b>{{$details['evaluator_password']}}</b> <br>
+1. That's your dashboard. Go to <b>{{$details['competition_name']}}</b> <br>
 
 Thanks,<br>
 {{ config('app.name') }}
